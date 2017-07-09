ac93025e6892b8e15352319424e3d3a9b3839142
clypes@ymail.com <martins.mezajevs@gmail.com>
2017-07-10 01:21:47 +0300

    Its been some time...

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a+b);
+            var name = System.Environment.MachineName;
+            Console.WriteLine(name);
+        }
+    }
+}
